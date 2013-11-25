cd9e1b49d7804a750a76f35ebf0ce8875afadbab
Tobias Rausch <rauschtobi@gmail.com>
2013-11-25 22:50:23 +0100

    Added SV site filters.

--- src/diffRC.py
+++ src/diffRC.py
@@ -14,8 +14,22 @@ parser=argparse.ArgumentParser(description='Annotates sites for differential rea
 parser.add_argument('-v', '--vcf', metavar='variants.vcf', required=True, dest='vcfFile', help='input vcf file (required)')
 parser.add_argument('-out', '--out', metavar='out.vcf', required=True, dest='outFile', help='output vcf file (required)')
 parser.add_argument('-t', '--type', metavar='DEL', required=True, dest='svType', help='SV type [DEL, DUP, INV] (required)')
-parser.add_argument('-p', '--pvalue', metavar='1.0', required=False, dest='pValueCut', help='p-value threshold to exclude SVs')
+parser.add_argument('-p', '--pvalue', metavar='1.0', required=False, dest='pValueCut', help='p-value threshold for read-depth')
+parser.add_argument('-w', '--hwe', metavar='1.0', required=False, dest='hweCut', help='p-value threshold for HWEpval')
+parser.add_argument('-s', '--size', metavar='0', required=False, dest='sizeCut', help='min. required size of SVs')
+parser.add_argument('-f', '--filter', dest='siteFilter', action='store_true', help='Filter sites for PASS')
 args = parser.parse_args()
+
+# Set parameters
+sizeCut=0
+if (args.sizeCut):
+    sizeCut=int(args.sizeCut)
+hweCut=1.0
+if (args.hweCut):
+    hweCut=float(args.hweCut)
+pValueCut=1.0
+if (args.pValueCut):
+    pValueCut=float(args.pValueCut)
 
 # Parse vcf File
 if args.vcfFile:
@@ -26,9 +40,9 @@ if args.vcfFile:
         rcRef=[]
         rcAlt=[]
         for call in record.samples:
-            if ((call['FT']=="PASS") and (call.gt_type==0)):
+            if ((call.called) and (call['FT']=="PASS") and (call.gt_type==0)):
                 rcRef.append(call['RC'])
-            if (call.gt_type!=0):
+            if ((call.called) and (call.gt_type!=0)):
                 rcAlt.append(call['RC'])
         wilcoxonPval=1.0
         if ((len(rcRef)>0) and (len(rcAlt)>0)):
@@ -39,5 +53,7 @@ if args.vcfFile:
                 wilcoxonPval=wilcoxonStat[1]
             else:
                 wilcoxonPval=wilcoxonStat[1]
-        record.INFO['RDpval']=round(wilcoxonPval,6)
-        vcf_writer.write_record(record)
+        record.INFO['RDpval']=float(round(wilcoxonPval,6))
+        if ((record.INFO['RDpval'] <= pValueCut) and (record.INFO['SVLEN']>=sizeCut) and (record.INFO['HWEpval'] <= hweCut)):
+            if ((not args.siteFilter) or (len(record.FILTER)==0)):
+                vcf_writer.write_record(record)
