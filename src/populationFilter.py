624df25e2aa20d0f8cef5ef0e2d7c7137f77b03a
Tobias Rausch <rauschtobi@gmail.com>
2014-02-06 15:51:52 +0100

    Updated population filter.

--- src/populationFilter.py
+++ src/populationFilter.py
@@ -1,33 +1,28 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
-import scipy.stats
 import vcf
 import argparse
-import csv
-import sys
-import collections
 import numpy
-import re
 import banyan
 
 
 #Functions
 def overlapValid(s1, e1, s2, e2, reciprocalOverlap=0.5, maxOffset=500):
-    if ((e1<s2) or (s1>e2)):
+    if (e1 < s2) or (s1 > e2):
         return False
-    overlapLen=float(min(e1,e2) - max(s1,s2))
+    overlapLen = float(min(e1, e2) - max(s1, s2))
     # Check reciprocal overlap
-    if ((overlapLen<1) or (float(e1-s1)/overlapLen<reciprocalOverlap) or (float(e2-s2)/overlapLen<reciprocalOverlap)):
+    if (overlapLen < 1) or (float(e1-s1)/overlapLen < reciprocalOverlap) or (float(e2-s2)/overlapLen < reciprocalOverlap):
         return False
     # Check offset
-    if ((abs(s2-s1)>maxOffset) or (abs(e2-e1)>maxOffset)):
+    if (abs(s2-s1) > maxOffset) or (abs(e2-e1) > maxOffset):
         return False
     return True
 
 
 # Parse command line
-parser=argparse.ArgumentParser(description='Filter for reliable SV sites.')
+parser = argparse.ArgumentParser(description='Filter for reliable SV sites.')
 parser.add_argument('-v', '--vcf', metavar='variants.vcf', required=True, dest='vcfFile', help='input vcf file (required)')
 parser.add_argument('-o', '--out', metavar='out.vcf', required=True, dest='outFile', help='output vcf file (required)')
 parser.add_argument('-g', '--gq', metavar='30', required=False, dest='gqCut', help='min. GQ (optional)')
@@ -38,69 +33,69 @@ parser.add_argument('-f', '--filter', dest='siteFilter', action='store_true', he
 args = parser.parse_args()
 
 # Command-line args
-sampleID=""
+sampleID = ""
 if args.sampleID:
-    sampleID=args.sampleID
-gqCut=30
+    sampleID = args.sampleID
+gqCut = 30
 if args.gqCut:
-    gqCut=int(args.gqCut)
-minSize=500
+    gqCut = int(args.gqCut)
+minSize = 500
 if args.minSize:
-    minSize=int(args.minSize)
-maxSize=5000000
+    minSize = int(args.minSize)
+maxSize = 5000000
 if args.maxSize:
-    maxSize=int(args.maxSize)
+    maxSize = int(args.maxSize)
 
 # Collect high-quality SVs
-sv=dict()
+sv = dict()
 if args.vcfFile:
-    vcf_reader=vcf.Reader(open(args.vcfFile), 'r')
+    vcf_reader = vcf.Reader(open(args.vcfFile), 'r')
     for record in vcf_reader:
-        if ((not args.siteFilter) or (len(record.FILTER)==0)):
-            gqRef=[]
-            gqAlt=[]
-            ratioRef=[]
-            ratioAlt=[]
-            carrierSample=False
-            if (sampleID==""):
-                carrierSample=True
+        if (not args.siteFilter) or (len(record.FILTER) == 0):
+            gqRef = []
+            gqAlt = []
+            ratioRef = []
+            ratioAlt = []
+            carrierSample = False
+            if sampleID == "":
+                carrierSample = True
             for call in record.samples:
-                if ((call.called) and (call['FT']=="PASS")):
-                    if (call.gt_type==0):
-                        if (call['DV']==0):
+                if (call.called) and (call['FT'] == "PASS"):
+                    if call.gt_type == 0:
+                        if call['DV'] == 0:
                             gqRef.append(call['GQ'])
                         else:
                             ratioRef.append(float(call['DV'])/float(call['DR'] + call['DV']))
-                    if (call.gt_type!=0):
-                        if ((not carrierSample) and (call.sample==sampleID)):
-                            carrierSample=True
+                    if call.gt_type != 0:
+                        if (not carrierSample) and (call.sample == sampleID):
+                            carrierSample = True
                         gqAlt.append(call['GQ'])
                         ratioAlt.append(float(call['DV'])/float(call['DR'] + call['DV']))
-            if (carrierSample):
-                if ((len(gqRef)) and (len(gqAlt)) and (record.INFO['SVLEN']>=minSize) and (record.INFO['SVLEN']<=maxSize) and (numpy.median(gqRef)>=gqCut) and (numpy.median(gqAlt)>=gqCut)):
-                    if ((len(ratioRef)==0) and (numpy.median(ratioAlt)>=0.3)):
-                        if (not sv.has_key(record.CHROM)):
-                            sv[ record.CHROM ] = banyan.SortedDict(key_type=(int, int), alg=banyan.RED_BLACK_TREE, updator=banyan.OverlappingIntervalsUpdator)
-                        sv[ record.CHROM ][ (record.POS, record.INFO['END']) ]=(record.ID, record.INFO['PE'])
+            if carrierSample:
+                if (len(gqRef)) and (len(gqAlt)) and (record.INFO['SVLEN'] >= minSize) and (record.INFO['SVLEN'] <= maxSize) and (numpy.median(gqRef) >= gqCut) and (numpy.median(gqAlt) >= gqCut):
+                    if (len(ratioRef) == 0) and (numpy.median(ratioAlt) >= 0.3):
+                        if not sv.has_key(record.CHROM):
+                            sv[record.CHROM] = banyan.SortedDict(key_type=(int, int), alg=banyan.RED_BLACK_TREE, updator=banyan.OverlappingIntervalsUpdator)
+                        sv[record.CHROM][(record.POS, record.INFO['END'])] = (record.ID, record.INFO['PE'])
 
 # Output vcf records
 if args.vcfFile:
-    vcf_reader=vcf.Reader(open(args.vcfFile), 'r')
+    vcf_reader = vcf.Reader(open(args.vcfFile), 'r')
     vcf_writer = vcf.Writer(open(args.outFile, 'w'), vcf_reader, lineterminator='\n')
     for record in vcf_reader:
-        if ((record.CHROM not in sv.keys()) or ((record.POS, record.INFO['END']) not in sv[ record.CHROM ].keys())):
+        if (record.CHROM not in sv.keys()) or ((record.POS, record.INFO['END']) not in sv[record.CHROM].keys()):
             continue
-        overlapList=sv[ record.CHROM ].overlap((record.POS, record.INFO['END']))
-        svID, score=sv[ record.CHROM ][(record.POS, record.INFO['END'])]
-        foundBetterHit=False
+        overlapList = sv[record.CHROM].overlap((record.POS, record.INFO['END']))
+        svID, score = sv[record.CHROM][(record.POS, record.INFO['END'])]
+        foundBetterHit = False
         for cStart, cEnd in overlapList:
-            cSvID, cScore=sv[ record.CHROM ][(cStart, cEnd)]
-            if (svID==cSvID):
-                continue 
-            if (not overlapValid(record.POS, record.INFO['END'], cStart, cEnd, 0.1, 10000000)):  
+            cSvID, cScore = sv[record.CHROM][(cStart, cEnd)]
+            if svID == cSvID:
                 continue
-            if ((cScore>score) or ((cScore==score) and (cSvID<svID))):
-                foundBetterHit=True
+            if not overlapValid(record.POS, record.INFO['END'], cStart, cEnd, 0.1, 10000000):
+                continue
+            if (cScore > score) or ((cScore == score) and (cSvID < svID)):
+                foundBetterHit = True
                 break
-        if (not foundBetterHit):
+        if not foundBetterHit:
             vcf_writer.write_record(record)
