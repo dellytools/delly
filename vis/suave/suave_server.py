5f209b8586661252287d07dc1d6aecdd11f4ae82
mhyfritz <markus.h.y.fritz@gmail.com>
2015-01-19 17:57:09 +0100

    Flesh out server

--- vis/suave/suave_server.py
+++ vis/suave/suave_server.py
@@ -2,24 +2,83 @@
 
 # Copyright (c) 2014 Markus Hsi-Yang Fritz
 
+from __future__ import division
 import click
 from flask import Flask, render_template
+import h5py
+import os
+import json
+from collections import defaultdict
+import numpy as np
 
 app = Flask(__name__)
-h5_fns = []
+cfg = dict()
+
+
+def get_smpl_names(h5_fns, use_fns):
+    if use_fns:
+        return [os.path.basename(fn) for fn in h5_fns]
+    ss = []
+    for h5_fn in h5_fns:
+        with h5py.File(h5_fn) as f:
+            ss.append(f.attrs['sample'])
+    return ss
+
+
+def get_common_chroms(s1, s2):
+    with h5py.File(cfg['smpl_to_file'][s1]) as f:
+        chroms1 = set(c for c in f)
+    with h5py.File(cfg['smpl_to_file'][s2]) as f:
+        chroms2 = set(c for c in f)
+    return sorted(list(chroms1 & chroms2))
+
+
+@app.route('/chroms/<s1>/<s2>')
+def chroms(s1, s2):
+    return json.dumps(get_common_chroms(s1, s2))
+
+
+@app.route('/data/<s1>/<s2>/<c>')
+def data(s1, s2, c):
+    d = {
+        'chrom': c,
+        'chrom_len': -1,
+    }
+    with h5py.File(cfg['smpl_to_file'][s1]) as f1, \
+            h5py.File(cfg['smpl_to_file'][s2]) as f2:
+        assert c in f1 and c in f2
+        d['chrom_len'] = int(f1[c].attrs['length'])
+        x = f1['/{}/read_counts'.format(c)][:]
+        y = f2['/{}/read_counts'.format(c)][:]
+        d['ratios'] = np.log2(x/y).tolist()
+
+    return json.dumps(d)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    smpl_names = get_smpl_names(cfg['h5_fns'], cfg['use_fns'])
+    cfg['smpl_to_file'] = dict()
+    for smpl, fn in zip(smpl_names, cfg['h5_fns']):
+        cfg['smpl_to_file'][smpl] = fn
+    chroms = get_common_chroms(smpl_names[0], smpl_names[0])
+    return render_template('index.html',
+                           samples=smpl_names,
+                           chromosomes=chroms)
+
 
 @click.command()
 @click.option('-p', '--port', default=5000, help='port number')
-@click.option('-d', '--debug', default=False, help='run server in debug mode')
+@click.option('--debug/--no-debug', default=False,
+              help='run server in debug mode')
 @click.option('h5s', '-f', '--file', multiple=True, required=True,
-              help='HDF5 coverage file')
-def cli(port, debug, h5s):
-    global h5_fns
-    h5_fns = list(h5s)
+              type=click.Path(exists=True), help='HDF5 coverage file')
+@click.option('--usefilenames/--no-usefilenames', default=False,
+              help='use file names as sample names')
+def cli(port, debug, h5s, usefilenames):
+    global cfg
+    cfg['h5_fns'] = list(h5s)
+    cfg['use_fns'] = usefilenames
     app.run(port=port, debug=debug)
 
 if __name__ == '__main__':
