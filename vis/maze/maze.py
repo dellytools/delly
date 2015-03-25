d26dee31bac8bbd00b810063383d70b96e349c13
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2015-03-25 16:14:09 +0100

    Add MUMmer3 call & match parsing

--- vis/maze/maze.py
+++ vis/maze/maze.py
@@ -1,7 +1,24 @@
-def mummer_matches(fn1, fn2, l, m):
-    pass
+from __future__ import print_function
+import re
+from subprocess import check_output
 
-def mummer_matches_seqs(s1, s2, l, m):
-    # make temp files
-    mummer_matches(fn1, fn2, l, m)
-    # delete temp files
+m_pat = re.compile(r'\s*(\d+)\s+(\d+)\s+(\d+)')
+
+def mummer_matches(fn1, fn2, l, m, verbose=False):
+    mode = ' -maxmatch' if m == 'mem' else ''
+    cmd = 'mummer{} -l {} -b -c {} {} 2>/dev/null'.format(mode, l, fn1, fn2)
+    if verbose:
+        print('#', cmd)
+    mummer_out = check_output(cmd, shell=True)
+    fwd = []
+    rev = []
+    for l in mummer_out.splitlines():
+        if l.startswith('>'):
+            is_rev = True if 'Reverse' in l else False
+        else:
+            s1, s2, l = [int(x) for x in m_pat.match(l).group(1,2,3)]
+            if is_rev:
+                rev.append([s1, s1+l-1, s2, s2-l+1])
+            else:
+                fwd.append([s1, s1+l-1, s2, s2+l-1])
+    return {'fwd': fwd, 'rev': rev}
--- vis/maze/maze_server.py
+++ vis/maze/maze_server.py
@@ -2,10 +2,15 @@
 
 # 2014-2015 Markus Hsi-Yang Fritz
 
-from __future__ import division
+from __future__ import print_function, division
 import click
 from flask import Flask, render_template
+from tempfile import NamedTemporaryFile
+import os
+import gzip
 import json
+from readfq import readfq
+import maze
 
 app = Flask(__name__)
 cfg = {}
@@ -14,11 +19,43 @@ cfg = {}
 def index():
     return render_template('index.html')
 
+def opener(fn):
+    if fn.endswith('.gz'):
+        return gzip.open
+    return open
+
 @app.route('/data')
 def data():
-    import time
-    time.sleep(3)
-    return json.dumps({'foo': 'bar'})
+    d = []
+    with opener(cfg['reference'])(cfg['reference']) as f:
+        rname, rseq, _ = next(readfq(f))
+        with NamedTemporaryFile(delete=False) as f_ref:
+            fn_ref = f_ref.name
+            print('>{}'.format(rname), file=f_ref)
+            print(rseq, file=f_ref)
+    with opener(cfg['query'])(cfg['query']) as f:
+        for qname, qseq, _ in readfq(f):
+            with NamedTemporaryFile(delete=False) as f_query:
+                fn_query = f_query.name
+                print('>{}'.format(qname), file=f_query)
+                print(qseq, file=f_query)
+            matches = maze.mummer_matches(fn_ref,
+                                          fn_query,
+                                          cfg['length'],
+                                          cfg['matches'],
+                                          cfg['debug'])
+            d.append({
+                'rname': rname,
+                'rseq': rseq,
+                'qname': qname,
+                'qseq': qseq,
+                'matches': matches
+            })
+            os.remove(fn_query)
+    os.remove(fn_ref)
+    return json.dumps(d)
+
+# FIXME remove -l/-m and add to GUI
 
 @click.command()
 @click.option('-p', '--port', default=5000, help='port number')
@@ -40,7 +77,8 @@ def cli(port, debug, reference, query, coords, length, matches):
         'reference': reference,
         'query': query,
         'length': length,
-        'matches': matches
+        'matches': matches,
+        'debug': debug
     }
     app.run(port=port, debug=debug)
 
