c5fba383bfdaf5beb4b1cef40a96a6a9d906adda
tobiasrausch <rauschtobi@gmail.com>
2015-06-08 13:55:13 +0200

    fixed bug in insert size script

--- R/isize.R
+++ R/isize.R
@@ -1,9 +1,12 @@
 library(ggplot2)
+library(scales)
 library(grid)
 
 args=commandArgs(trailingOnly=TRUE)
 orient=c("F+", "F-", "R+", "R-")
 ins=read.table(args[1], header=T)
+maxSize = max(ins$Size)
+if ((length(args)>1) & (as.integer(args[2])<maxSize)) {  maxSize = as.integer(args[2]); }
 count=c()
 titleVal="Insert Size ("
 for (o in orient) {
@@ -22,8 +25,9 @@ sth=theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), le
 png(paste(args[1], "png", sep="."), height=800, width=800)
 grid.newpage()
 pushViewport(viewport(layout=grid.layout(4,2)))
-p1=ggplot(data=ins) + geom_histogram(aes(x=Size, y=Count, colour=Orientation, fill=Orientation), stat="identity") + ggtitle(titleVal) + sth
-p2=ggplot(data=ins) + geom_histogram(aes(x=Size, y=Count, colour=Orientation, fill=Orientation), stat="identity") + facet_wrap(~ Orientation, nrow=2, ncol=2) + sth
+p1=ggplot(data=ins, aes(x=Size, y=Count)) + geom_freqpoly(aes(colour=Orientation), size=1.2, stat="identity") + scale_y_continuous(labels=comma) + xlim(0, maxSize) + ggtitle(titleVal) + sth
+#p1=ggplot(data=ins, aes(x=Size, y=Count)) + geom_freqpoly(aes(colour=Orientation), size=1.2, stat="identity") + scale_y_log10(labels=comma) + xlim(0, maxSize) + ggtitle(titleVal) + sth
+p2=ggplot(data=ins, aes(x=Size, y=Count)) + geom_freqpoly(aes(colour=Orientation), size=1.2, stat="identity") + scale_y_continuous(labels=comma) + xlim(0, maxSize) + facet_wrap(~ Orientation, nrow=2, ncol=2) + sth
 print(p1, vp = viewport(layout.pos.row=1:2, layout.pos.col=1:2))
 print(p2, vp = viewport(layout.pos.row=3:4, layout.pos.col=1:2))
 dev.off()
