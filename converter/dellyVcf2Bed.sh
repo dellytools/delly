b773ced249862164cb7093820ced4f3648d11312
tobiasrausch <rauschtobi@gmail.com>
2015-08-27 17:06:44 +0200

    added translocations to vcf2bed converter

--- converter/dellyVcf2Bed.sh
+++ converter/dellyVcf2Bed.sh
@@ -7,5 +7,5 @@ then
 fi
 
 
-vcf-sort ${1} | grep -v "^#" | cut -f 1,2,3,8 | sed 's/\t[^\t]*;END=/\t/' | sed 's/;.*$//' | awk '{print $1"\t"$2"\t"$4"\t"$3;}'
-
+vcf-sort ${1} | grep -v "^#" | grep -v "SVTYPE=TRA" | cut -f 1,2,3,8 | sed 's/\t[^\t]*;END=/\t/' | sed 's/;.*$//' | awk '{print $1"\t"$2"\t"$4"\t"$3;}'
+vcf-sort ${1} | grep -v "^#" | grep "SVTYPE=TRA" | cut -f 1,2,3,8 | sed 's/\t[^\t]*CHR2=/\t/' | sed 's/;[^\t]*END=/\t/' | sed 's/;.*$//' | awk '{print $1"\t"($2-500)"\t"($2+500)"\t"$3"A\n"$4"\t"($5-500)"\t"($5+500)"\t"$3"B";}'
