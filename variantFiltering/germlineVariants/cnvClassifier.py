9e46d086b115133904363b08d665d9c7d8a8c9b1
tobiasrausch <rauschtobi@gmail.com>
2015-09-06 14:54:12 +0200

    adapted cnvfilter for RCL and RCR

--- variantFiltering/germlineVariants/cnvClassifier.py
+++ variantFiltering/germlineVariants/cnvClassifier.py
@@ -18,22 +18,8 @@ import re
 parser = argparse.ArgumentParser(description='Deletion/Duplication filter.')
 parser.add_argument('-v', '--vcf', metavar='cnv.vcf', required=True, dest='cnvVCF', help='deletion/duplication vcf file (required)')
 parser.add_argument('-o', '--outVCF', metavar='out.vcf', required=True, dest='outVCF', help='output vcf file (required)')
-parser.add_argument('-r', '--readDepth', dest='readDepth', action='store_true', help='Filter sites according to read-depth')
 parser.add_argument('-f', '--filter', dest='siteFilter', action='store_true', help='Filter sites for PASS')
 args = parser.parse_args()
-
-# Parse control deletions/duplications
-sv = dict()
-if args.cnvVCF:
-    vcf_reader = vcf.Reader(open(args.cnvVCF), 'r', compressed=True) if args.cnvVCF.endswith('.gz') else vcf.Reader(open(args.cnvVCF), 'r', compressed=False)
-    for record in vcf_reader:
-        if ('CONTROL' not in record.INFO.keys()) or (record.INFO['CONTROL'] == 0):
-            continue
-        if record.INFO['CONTROL'] not in sv.keys():
-            sv[record.INFO['CONTROL']] = collections.defaultdict(int)
-        for call in record.samples:
-            if call.called:
-                sv[record.INFO['CONTROL']][call.sample] += call['RC']
 
 # Parse deletions/duplications
 cnvRegion = dict()
@@ -41,36 +27,36 @@ G = networkx.Graph()
 if args.cnvVCF:
     vcf_reader = vcf.Reader(open(args.cnvVCF), 'r', compressed=True) if args.cnvVCF.endswith('.gz') else vcf.Reader(open(args.cnvVCF), 'r', compressed=False)
     for record in vcf_reader:
-        if ('CONTROL' in record.INFO.keys()) and (record.INFO['CONTROL'] != 0):
-            continue
         if (not args.siteFilter) or (len(record.FILTER) == 0):
-            hap = dict()
-            rc = collections.defaultdict(int)
+            hetRC = list()
+            refRC = list()
             peCount = 0
             for call in record.samples:
                 if call.called:
-                    rc[call.sample] = call['RC']
-                    hap[call.sample] = [int(gVal) for gVal in call['GT'].split('/')]
+                    if (call['RC'] > 0) and (call['RCL'] + call['RCR'] > 0):
+                        hap = [int(gVal) for gVal in call['GT'].split('/')]
+                        if sum(hap) == 0:
+                            refRC.append(float(call['RC'])/float(call['RCL'] + call['RCR']))
+                        elif sum(hap) == 1:
+                            hetRC.append(float(call['RC'])/float(call['RCL'] + call['RCR']))
                     if (call.gt_type != 0) and (call['DV'] > 0):
                         peCount += call['DV']
-            if len(hap):
+            if (len(hetRC)) and (len(refRC)):
                 svStart = record.POS
                 svEnd = record.INFO['END']
-                svControlID = re.sub(r"^[A-Z0]*","", record.ID)
-                rdRatio = altRefReadDepthRatio(rc, sv[int(svControlID)], hap)
+                rdRatio = numpy.median(numpy.array(hetRC))/numpy.median(numpy.array(refRC))
                 #print(record.CHROM, svStart, svEnd, record.ID, rdRatio, sep="\t")
-                if rdRatio is not None:
-                    if ((record.INFO['SVTYPE'] == "DEL") and (rdRatio < 0.8)) or ((record.INFO['SVTYPE'] == "DUP") and (rdRatio >= 1.3) and (rdRatio <= 1.75)):
-                        # Valid Call
-                        if not cnvRegion.has_key(record.CHROM):
-                            cnvRegion[record.CHROM] = banyan.SortedDict(key_type=(int, int), alg=banyan.RED_BLACK_TREE, updator=banyan.OverlappingIntervalsUpdator)
-                        G.add_node(record.ID)
-                        G.node[record.ID]['Score'] = peCount
-                        for cnvIStart, cnvIEnd in cnvRegion[record.CHROM].overlap((svStart, svEnd)):
-                            otherID = cnvRegion[record.CHROM][(cnvIStart, cnvIEnd)]
-                            if overlapValid((svStart, svEnd), (cnvIStart, cnvIEnd), 0.1, 10000):
-                                G.add_edge(record.ID, otherID)
-                        cnvRegion[record.CHROM][(svStart, svEnd)] = record.ID
+                if ((record.INFO['SVTYPE'] == "DEL") and (rdRatio < 0.8)) or ((record.INFO['SVTYPE'] == "DUP") and (rdRatio >= 1.3) and (rdRatio <= 1.75)):
+                    # Valid Call
+                    if not cnvRegion.has_key(record.CHROM):
+                        cnvRegion[record.CHROM] = banyan.SortedDict(key_type=(int, int), alg=banyan.RED_BLACK_TREE, updator=banyan.OverlappingIntervalsUpdator)
+                    G.add_node(record.ID)
+                    G.node[record.ID]['Score'] = peCount
+                    for cnvIStart, cnvIEnd in cnvRegion[record.CHROM].overlap((svStart, svEnd)):
+                        otherID = cnvRegion[record.CHROM][(cnvIStart, cnvIEnd)]
+                        if overlapValid((svStart, svEnd), (cnvIStart, cnvIEnd), 0.1, 10000):
+                            G.add_edge(record.ID, otherID)
+                    cnvRegion[record.CHROM][(svStart, svEnd)] = record.ID
 
 # Pick best deletion/duplication for all overlapping calls
 selectedSVs = set()
