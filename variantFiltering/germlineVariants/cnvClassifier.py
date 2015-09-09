4059fde2cf7cd1cb4ce27fdba765ecab8609dfd0
tobiasrausch <rauschtobi@gmail.com>
2015-09-09 14:15:33 +0200

    refined CNV filtering

--- variantFiltering/germlineVariants/cnvClassifier.py
+++ variantFiltering/germlineVariants/cnvClassifier.py
@@ -28,35 +28,42 @@ if args.cnvVCF:
     vcf_reader = vcf.Reader(open(args.cnvVCF), 'r', compressed=True) if args.cnvVCF.endswith('.gz') else vcf.Reader(open(args.cnvVCF), 'r', compressed=False)
     for record in vcf_reader:
         if (not args.siteFilter) or (len(record.FILTER) == 0):
+            rc = list()
             hetRC = list()
             refRC = list()
+            gqAlt = list()
             peCount = 0
             for call in record.samples:
                 if call.called:
-                    if (call['RC'] > 0) and (call['RCL'] + call['RCR'] > 0):
-                        hap = [int(gVal) for gVal in call['GT'].split('/')]
-                        if sum(hap) == 0:
+                    hap = [int(gVal) for gVal in call['GT'].split('/')]
+                    if (sum(hap) == 0):
+                        rc.append(call['RC'])
+                        if call['RCL'] + call['RCR'] > 0:
                             refRC.append(float(call['RC'])/float(call['RCL'] + call['RCR']))
-                        elif sum(hap) == 1:
-                            hetRC.append(float(call['RC'])/float(call['RCL'] + call['RCR']))
-                    if (call.gt_type != 0) and (call['DV'] > 0):
+                    else:
+                        gqAlt.append(call['GQ'])
                         peCount += call['DV']
+                        if (sum(hap) == 1) and (call['RCL'] + call['RCR'] > 0):
+                            hetRC.append(float(call['RC'])/float(call['RCL'] + call['RCR']))
             if (len(hetRC)) and (len(refRC)):
                 svStart = record.POS
                 svEnd = record.INFO['END']
                 rdRatio = numpy.median(numpy.array(hetRC))/numpy.median(numpy.array(refRC))
-                #print(record.CHROM, svStart, svEnd, record.ID, rdRatio, sep="\t")
-                if ((record.INFO['SVTYPE'] == "DEL") and (rdRatio < 0.8)) or ((record.INFO['SVTYPE'] == "DUP") and (rdRatio >= 1.3) and (rdRatio <= 1.75)):
-                    # Valid Call
-                    if not cnvRegion.has_key(record.CHROM):
-                        cnvRegion[record.CHROM] = banyan.SortedDict(key_type=(int, int), alg=banyan.RED_BLACK_TREE, updator=banyan.OverlappingIntervalsUpdator)
-                    G.add_node(record.ID)
-                    G.node[record.ID]['Score'] = peCount
-                    for cnvIStart, cnvIEnd in cnvRegion[record.CHROM].overlap((svStart, svEnd)):
-                        otherID = cnvRegion[record.CHROM][(cnvIStart, cnvIEnd)]
-                        if overlapValid((svStart, svEnd), (cnvIStart, cnvIEnd), 0.1, 10000):
-                            G.add_edge(record.ID, otherID)
-                    cnvRegion[record.CHROM][(svStart, svEnd)] = record.ID
+                covIndex = float(record.INFO['CIPOS'][1]) / float(numpy.median(numpy.array(rc)))
+                altgq = numpy.median(numpy.array(gqAlt))
+                #print(record.CHROM, svStart, svEnd, record.ID, rdRatio, covIndex, altgq, sep="\t")
+                if (covIndex < 0.75) and (altgq > 25):
+                    if ((record.INFO['SVTYPE'] == "DEL") and (rdRatio < 0.8)) or ((record.INFO['SVTYPE'] == "DUP") and (rdRatio >= 1.3) and (rdRatio <= 1.75)):
+                        # Valid Call
+                        if not cnvRegion.has_key(record.CHROM):
+                            cnvRegion[record.CHROM] = banyan.SortedDict(key_type=(int, int), alg=banyan.RED_BLACK_TREE, updator=banyan.OverlappingIntervalsUpdator)
+                        G.add_node(record.ID)
+                        G.node[record.ID]['Score'] = peCount
+                        for cnvIStart, cnvIEnd in cnvRegion[record.CHROM].overlap((svStart, svEnd)):
+                            otherID = cnvRegion[record.CHROM][(cnvIStart, cnvIEnd)]
+                            if overlapValid((svStart, svEnd), (cnvIStart, cnvIEnd), 0.1, 10000):
+                                G.add_edge(record.ID, otherID)
+                        cnvRegion[record.CHROM][(svStart, svEnd)] = record.ID
 
 # Pick best deletion/duplication for all overlapping calls
 selectedSVs = set()
