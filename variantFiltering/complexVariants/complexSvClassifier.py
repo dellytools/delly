1deb65252eb4a3af76f263e4bff0d616676af105
tobiasrausch <rauschtobi@gmail.com>
2015-09-15 09:01:15 +0200

    fixed bug in complexSvClassifier

--- variantFiltering/complexVariants/complexSvClassifier.py
+++ variantFiltering/complexVariants/complexSvClassifier.py
@@ -118,8 +118,6 @@ if args.invVCF:
                                 if (record.INFO['SVTYPE'] != 'INV') and (updSVType != "DUP"):
                                     continue
                                 if (updSVType != 'DUP') or (maxBpOffset > minDuplicationLength):
-                                    print((s1,e1), nonRefHap, rc)
-                                    print((s2,e2), inv3to3['hap'], inv3to3['rc'])
                                     score = float(min(support, inv3to3['sup'])) * float(cc)
                                     if score > invInfo['score']:
                                         invInfo = {'id': inv3to3['id'], 'start': min(s1, s2), 'end': max(e1, e2), 'score': score}
@@ -153,6 +151,9 @@ if args.invVCF:
     vcf_writer = vcf.Writer(open(args.outVCF, 'w'), vcf_reader, lineterminator='\n')
     for record in vcf_reader:
         if record.ID in set(reduce(operator.add, map(list, idPairs.keys()))):
+            precise = False
+            if 'PRECISE' in record.INFO.keys():
+                precise = record.INFO['PRECISE']
             ci = max(abs(record.INFO['CIEND'][0]), abs(record.INFO['CIEND'][1]), abs(record.INFO['CIPOS'][0]), abs(record.INFO['CIPOS'][1]))
             svm = record.INFO['SVMETHOD']
             svtinv = record.INFO['SVTYPE']
@@ -187,8 +188,6 @@ if args.complexVCF:
             ((s1, e1), (s2, e2)) = ((selectedSVs[id1]['start'], selectedSVs[id1]['end']), (selectedSVs[id2]['start'], selectedSVs[id2]['end']))
             (recO, nestedO, recUnion, bpOffset, oLen) = overlapMetrics((s1, e1), (s2, e2))
             rdRatio = rdAltRefRatio(((s1, e1), (s2, e2)), (selectedSVs[id1]['nonRefHap'], selectedSVs[id2]['nonRefHap']), (selectedSVs[id1]['rc'], selectedSVs[id2]['rc']))
-            print((s1,e1), selectedSVs[id1]['nonRefHap'], selectedSVs[id1]['rc'])
-            print((s2,e2), selectedSVs[id2]['nonRefHap'], selectedSVs[id2]['rc'])
             updSVType = validRdRatio(recO/nestedO, rdRatio)[1]
 
             # Separate the 2 SVs
