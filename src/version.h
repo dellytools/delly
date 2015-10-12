c60c09a703d1bb195afaef1d074d027222198cd7
tobiasrausch <rauschtobi@gmail.com>
2015-10-12 10:24:42 +0200

    version 0.7.1

--- src/version.h
+++ src/version.h
@@ -27,7 +27,7 @@ Contact: Tobias Rausch (rausch@embl.de)
 namespace torali
 {
 
-  std::string dellyVersionNumber = "0.6.8";
+  std::string dellyVersionNumber = "0.7.1";
 
   inline 
     void printTitle(std::string const& title) 
