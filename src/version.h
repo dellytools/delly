b33611d5d0bf0834dbf35cf501fe3abb5a19b63e
tobiasrausch <rauschtobi@gmail.com>
2016-04-18 09:13:19 +0200

    delly v0.7.3

--- src/version.h
+++ src/version.h
@@ -27,7 +27,7 @@ Contact: Tobias Rausch (rausch@embl.de)
 namespace torali
 {
 
-  std::string dellyVersionNumber = "0.7.2";
+  std::string dellyVersionNumber = "0.7.3";
 
   inline 
     void printTitle(std::string const& title) 
