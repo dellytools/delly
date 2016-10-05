e114896cfe0f3a53123eb5b64cb1a51487213ef5
tobiasrausch <rauschtobi@gmail.com>
2016-10-05 12:01:14 +0200

    split-read refinement updates REF

--- src/split.h
+++ src/split.h
@@ -605,6 +605,10 @@ namespace torali
       std::string altPart = precChar;
       if (ad.cEnd > ad.cStart + 1) altPart += sv.consensus.substr(ad.cStart, (ad.cEnd - ad.cStart) - 1);
       sv.alleles = refPart + "," + altPart;
+    } else {
+      std::string precChar = svRefStr.substr(ad.rStart - 1, 1);
+      std::string refPart = precChar;
+      sv.alleles = refPart + ",<" + _addID(svType) + ">";
     }
     
     return true;
