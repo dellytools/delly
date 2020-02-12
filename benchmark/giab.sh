897128dd9ad81d04e02e0bbed6d475399208536e
tobiasrausch <rauschtobi@gmail.com>
2020-02-12 09:09:03 +0100

    added pacbio

--- benchmark/giab.sh
+++ benchmark/giab.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-# Install tools
-make all
-make test
-if [ $? -ne 0 ]
-then
-    echo ""
-    echo "Please install dependencies first using 'make all'"
-    exit -1;
-fi
-
 # Download GIAB data set
 if [ ! -f hs37d5.fa.gz ]
 then
     wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
-fi
-if [ ! -f hs37d5.fa.gz.fai ]
-then
     samtools faidx hs37d5.fa.gz
 fi
 if [ ! -f HG002_SVs_Tier1_v0.6.bed ]
@@ -30,10 +17,22 @@ fi
 if [ ! -f ultra-long-ont_hs37d5_phased.bam ]
 then
     wget ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/HG002_NA24385_son/Ultralong_OxfordNanopore/final/ultra-long-ont_hs37d5_phased.bam
+    samtools index ultra-long-ont_hs37d5_phased.bam
 fi
-if [ ! -f ultra-long-ont_hs37d5_phased.bam.bai ]
+if [ ! -f HG002.SequelII.pbmm2.hs37d5.whatshap.haplotag.RTG.10x.trio.bam ]
 then
-    wget ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/HG002_NA24385_son/Ultralong_OxfordNanopore/final/ultra-long-ont_hs37d5_phased.bam.bai
+    wget ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/HG002_NA24385_son/PacBio_SequelII_CCS_11kb/HG002.SequelII.pbmm2.hs37d5.whatshap.haplotag.RTG.10x.trio.bam
+    samtools index HG002.SequelII.pbmm2.hs37d5.whatshap.haplotag.RTG.10x.trio.bam
+fi
+
+# Install tools
+make all
+make test
+if [ $? -ne 0 ]
+then
+    echo ""
+    echo "Please install dependencies first using 'make all'"
+    exit -1;
 fi
 
 # Activate environment
