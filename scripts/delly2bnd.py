5d9806128e38055dacdb9e09e993bfd4a85fa82e
Tobias Rausch <rauschtobi@gmail.com>
2025-07-30 14:26:44 +0200

    readfq func

--- scripts/delly2bnd.py
+++ scripts/delly2bnd.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
-from .readfq import readfq
 import argparse
 import sys
 import cyvcf2
@@ -16,6 +15,39 @@ def argument_parsing():
     parser.add_argument('-r', '--ref', metavar='ref.fa', required=True, dest='ref', help='input reference (required)')
     parser.add_argument('-o', '--out', metavar='out.vcf', required=True, dest='out', help='output VCF file (required)')
     return parser.parse_args()
+
+"""Heng Li's FASTA/FASTQ parser
+"""
+def readfq(fp): # this is a generator function
+    last = None # this is a buffer keeping the last unprocessed line
+    while True: # mimic closure; is it a bad idea?
+        if not last: # the first record or a record following a fastq
+            for l in fp: # search for the start of the next record
+                if l[0] in '>@': # fasta/q header line
+                    last = l[:-1] # save this line
+                    break
+        if not last: break
+        name, seqs, last = last[1:].partition(" ")[0], [], None
+        for l in fp: # read the sequence
+            if l[0] in '@+>':
+                last = l[:-1]
+                break
+            seqs.append(l[:-1])
+        if not last or last[0] != '+': # this is a fasta record
+            yield name, ''.join(seqs), None # yield a fasta record
+            if not last: break
+        else: # this is a fastq record
+            seq, leng, seqs = ''.join(seqs), 0, []
+            for l in fp: # read the quality
+                seqs.append(l[:-1])
+                leng += len(l) - 1
+                if leng >= len(seq): # have read enough quality
+                    last = None
+                    yield name, seq, ''.join(seqs); # yield a fastq record
+                    break
+            if last: # reach EOF before reading enough quality
+                yield name, seq, None # yield a fasta record instead
+                break
 
 def delly2bnd(args):
     # Fetch breakpoint positions
